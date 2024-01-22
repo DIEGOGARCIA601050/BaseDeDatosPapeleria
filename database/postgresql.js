32bfb13ca6d3d735bf0d742e59ef50f99fd9b7f7
= <=ekrnal9@gmail.com>
2024-01-22 14:10:15 -0600

    Empezando el inventario

---
+++ database/postgresql.js
@@ -0,0 +1,9 @@
+import { Client } from 'pg';
+export const client = new Client({
+    user: 'fl0user',
+    host: 'ep-blue-wood-89329104.ap-southeast-1.aws.neon.fl0.io',
+    database: 'Login',
+    password: 'ePyRIhaS1u3l',
+    port: 5432,
+  });
+  
